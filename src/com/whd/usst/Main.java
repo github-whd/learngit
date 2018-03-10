25635424c697b0def6c32d705ea4d538359632d8
lenovo <Aa1991517>
2018-03-10 22:21:04 +0800

    wqeq

---
+++ src/com/whd/usst/Main.java
@@ -0,0 +1,22 @@
+package com.whd.usst;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /**
+         * 功能描述: <br>
+         * 〈〉
+         *
+         * @param args
+         * @return:void
+         * @since: 1.0.0
+         * @Author:lenovo
+         * @Date: 2018/3/9 21:47
+         */
+
+        // write your code here
+        System.out.println("Hello World!");
+        System.out.println("你好");
+
+    }
+}
