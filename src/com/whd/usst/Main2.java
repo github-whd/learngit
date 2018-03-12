66df46403c33e44e2bea7281cea3c709bcfa70f5
lenovo <Aa1991517>
2018-03-12 21:08:15 +0800

    一线

---
+++ src/com/whd/usst/Main2.java
@@ -0,0 +1,21 @@
+package com.whd.usst;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+        /**
+         * 功能描述: <br>
+         * 〈〉
+         *
+         * @param args
+         * @return:void
+         * @since: 1.0.0
+         * @Author:lenovo
+         * @Date: 2018/3/9 21:47
+         */
+
+        // write your code here
+        System.out.println("Hello World!");
+        System.out.println("上海");
+    }
+}
