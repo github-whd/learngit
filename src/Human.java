2d5763a00f6c062a0b3de1c09b090ee214e07b24
lenovo <Aa1991517>
2018-03-12 15:47:45 +0800

    更改

---
+++ src/Human.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: Human
+ * Author:   lenovo
+ * Date:     2018/3/9 21:21
+ * Description: people
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈people〉
+ *
+ * @author lenovo
+ * @create 2018/3/9
+ * @since 1.0.0
+ */
+
+/**
+ * Created by lenovo on 2018/3/9.
+ */
+public class Human {
+    public static void main(String[] args){
+        System.out.print("世界和平！");
+    }
+}
--- src/com/whd/usst/Main.java
+++ src/com/whd/usst/Main.java
@@ -17,6 +17,6 @@ public class Main {
         // write your code here
         System.out.println("Hello World!");
         System.out.println("你好");
-
+        System.out.println("你sssssssssssss好");
     }
 }
