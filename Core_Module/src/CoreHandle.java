b4e6cafd5ad85cf04f76f703b9d2a912c5d8f878
lenovo <Aa1991517>
2018-03-12 15:35:09 +0800

    wqeqfvc

---
+++ Core_Module/src/CoreHandle.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: CoreHandle
+ * Author:   lenovo
+ * Date:     2018/3/10 18:58
+ * Description: 核心处理类
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ *
+ *
+ * 〈一句话功能简述〉<br> 
+ * 〈核心处理类〉
+ *
+ * @author lenovo
+ * @create 2018/3/10
+ * @since 1.0.0
+ *
+ *
+ *
+ * Created by lenovo on 2018/3/10.
+ */
+public class CoreHandle {
+    public static void main(String[] args) {
+        System.out.println("jjjjjjj");
+    }
+}
